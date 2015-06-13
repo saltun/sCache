25d90ae2521109cfce60d2dbf9df10b0225469f4
Savaş Can ALTUN <savascanaltun@gmail.com>
2015-06-13 21:27:43 +0300

    Initial commit

---
+++ sCache.php
@@ -0,0 +1,119 @@
+<?php 
+/*
+* Author : Savaş Can Altun < savascanaltun@gmail.com >
+* Web : http://savascanaltun.com.tr
+* Mail : savascanaltun@gmail.com
+* GİT : http://github.com/saltun
+* Date : 13.06.2015
+* Update : 13.06.2015
+*/
+class sCache {
+	
+	private  $cache = null;
+	private  $time = 60;
+	private  $status = 0;
+	private  $dir = "sCache";
+	private  $buffer=false;
+	private  $start=null;
+	private  $load=false;
+
+	public function __construct($options=NULL){
+
+
+
+
+		if (isset($options)) {
+
+			if (is_array($options)) {
+
+				if(isset($options['dir']))    $this->dir = $options['dir'];
+				if(isset($options['buffer'])) $this->buffer = $options['buffer'];
+				if(isset($options['time']))   $this->time = $options['time'];
+				if(isset($options['load']))   $this->load = $options['load'];
+			
+				
+			}else{
+				die('Options only array');
+			}
+			
+		}
+
+		if(!file_exists($this->dir)){
+			mkdir($this->dir, 0777);
+		}
+
+		if ($this->load) {
+				list($time[1], $time[0]) = explode(' ', microtime());
+				$this->start = $time[1] + $time[0];
+		}
+
+		 $this->cache  =  dirname(__FILE__)."/".$this->dir."/".md5($_SERVER['REQUEST_URI']).".html";
+
+		 if(time() - $this->time < @filemtime($this->cache)) { 
+		      readfile($this->cache); 
+		      $this->status=1;
+		      die();
+		}else { 
+		 
+	      @unlink($this->cache); 
+
+		  ob_start();
+		}
+
+	}
+
+	private function buffer($buffer){
+
+		$buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
+		$buffer = str_replace(': ', ':', $buffer);
+		$buffer = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $buffer);
+
+		return $buffer;
+	}
+
+
+	private function writeCache($content){
+		$file = fopen($this->cache, 'w');
+		$content=$content."<!--- sCache Class - ".$this->time." Second -->";
+		@fwrite($file, $content);
+		fclose($file);
+
+
+	}
+
+	public function clearCache(){
+		$dir = opendir($this->dir); 
+		while (($file = readdir($dir)) !== false) 
+		{
+
+		if(! is_dir($file)){
+
+		  unlink($this->dir."/".$file);
+		}}
+		closedir($dir); 
+
+		
+	}
+
+	public function __destruct(){
+
+		if ($this->status==0) {
+			if ($this->buffer) {
+				$this->writeCache($this->buffer(ob_get_contents()));
+			}else{
+				$this->writeCache(ob_get_contents());
+			}
+			
+		}
+
+
+		if ($this->load) {
+				list($time[1], $time[0]) = explode(' ', microtime());
+				$finish = $time[1] + $time[0];
+				$total_time = number_format(($finish - $this->start), 6);
+				echo "Load Time (S) :  {$total_time} ";
+		}
+
+		ob_end_flush();
+	}
+}
