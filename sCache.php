53344b524501789431493e4e5ba861b3458b1d37
Savaş Can ALTUN <savascanaltun@gmail.com>
2015-06-21 23:35:08 +0300

    Update sCache.php
    externalpage options

--- sCache.php
+++ sCache.php
@@ -16,6 +16,8 @@ class sCache {
 	private  $buffer=false;
 	private  $start=null;
 	private  $load=false;
+	private  $external=array();
+	private  $type=true;
 
 	public function __construct($options=NULL){
 
@@ -26,10 +28,14 @@ class sCache {
 
 			if (is_array($options)) {
 
-				if(isset($options['dir']))    $this->dir = $options['dir'];
-				if(isset($options['buffer'])) $this->buffer = $options['buffer'];
-				if(isset($options['time']))   $this->time = $options['time'];
-				if(isset($options['load']))   $this->load = $options['load'];
+				if(isset($options['dir']))    	  $this->dir = $options['dir'];
+				if(isset($options['buffer'])) 	  $this->buffer = $options['buffer'];
+				if(isset($options['time']))   	  $this->time = $options['time'];
+				if(isset($options['load']))  	  $this->load = $options['load'];
+				if(isset($options['external']))   $this->external = $options['external'];
+
+
+		
 			
 				
 			}else{
@@ -38,28 +44,45 @@ class sCache {
 			
 		}
 
-		if(!file_exists($this->dir)){
-			mkdir($this->dir, 0777);
+		$myPage=explode('/',$_SERVER['REQUEST_URI']);
+		
+		foreach ($this->external as $key => $external) {
+			if (in_array(end($myPage), $this->external)) {
+				$this->type=false;
+				break;
+			}
 		}
 
-		if ($this->load) {
-				list($time[1], $time[0]) = explode(' ', microtime());
-				$this->start = $time[1] + $time[0];
+		if ($this->type) {
+		
+
+
+			if(!file_exists($this->dir)){
+				mkdir($this->dir, 0777);
+			}
+
+			if ($this->load) {
+					list($time[1], $time[0]) = explode(' ', microtime());
+					$this->start = $time[1] + $time[0];
+			}
+
+		
+			
+
+			 $this->cache  =  dirname(__FILE__)."/".$this->dir."/".md5($_SERVER['REQUEST_URI']).".html";
+
+			 if(time() - $this->time < @filemtime($this->cache)) { 
+			      readfile($this->cache); 
+			      $this->status=1;
+			      die();
+			}else { 
+			 
+		      @unlink($this->cache); 
+
+			  ob_start();
+			}
+
 		}
-
-		 $this->cache  =  dirname(__FILE__)."/".$this->dir."/".md5($_SERVER['REQUEST_URI']).".html";
-
-		 if(time() - $this->time < @filemtime($this->cache)) { 
-		      readfile($this->cache); 
-		      $this->status=1;
-		      die();
-		}else { 
-		 
-	      @unlink($this->cache); 
-
-		  ob_start();
-		}
-
 	}
 
 	private function buffer($buffer){
@@ -97,23 +120,26 @@ class sCache {
 
 	public function __destruct(){
 
-		if ($this->status==0) {
-			if ($this->buffer) {
-				$this->writeCache($this->buffer(ob_get_contents()));
-			}else{
-				$this->writeCache(ob_get_contents());
-			}
-			
+		if ($this->type) {
+
+				if ($this->status==0) {
+					if ($this->buffer) {
+						$this->writeCache($this->buffer(ob_get_contents()));
+					}else{
+						$this->writeCache(ob_get_contents());
+					}
+					
+				}
+
+
+				if ($this->load) {
+						list($time[1], $time[0]) = explode(' ', microtime());
+						$finish = $time[1] + $time[0];
+						$total_time = number_format(($finish - $this->start), 6);
+						echo "Load Time (S) :  {$total_time} ";
+				}
+
+				ob_end_flush();
 		}
-
-
-		if ($this->load) {
-				list($time[1], $time[0]) = explode(' ', microtime());
-				$finish = $time[1] + $time[0];
-				$total_time = number_format(($finish - $this->start), 6);
-				echo "Load Time (S) :  {$total_time} ";
-		}
-
-		ob_end_flush();
 	}
 }
